65984f5f78746b76198ce3bad0f517eb38094863
Yaroslav Gavrilovets <mir.lednev@gmail.com>
2020-10-02 12:29:41 +0300

    replaced numbers by variables

--- src/homework2/task1/Item4LongOverflow.java
+++ src/homework2/task1/Item4LongOverflow.java
@@ -4,8 +4,9 @@ public class Item4LongOverflow {
 
     public static void main(String[] args) {
         long a = 1L;
-        for (; a < Long.MAX_VALUE/3+1; a *= 3);
+        int multiplier = 3;
+        for (; a < Long.MAX_VALUE/multiplier+1; a *= multiplier);
         System.out.printf("Значение до переполнения = %d%n",a);
-        System.out.printf("Значение после переполнения = %d%n",a*3);
+        System.out.printf("Значение после переполнения = %d%n",a*multiplier);
     }
 }
--- src/homework2/task1/Item5MultiplicationTable.java
+++ src/homework2/task1/Item5MultiplicationTable.java
@@ -3,10 +3,14 @@ package homework2.task1;
 public class Item5MultiplicationTable {
 
     public static void main(String[] args) {
-        for (int k = 0; k < 2; k++) {
-            for (int i = 1; i < 11; i++) {
-                for (int j = 2; j < 6; j++) {
-                    System.out.printf("%3d x%2d=%2d",j+4*k,i,i*(j+4*k));
+        final int BLOCKS = 2;
+        final int INITIAL_VALUE = 2;
+        final int COLUMNS = 4;
+        final int MULTIPLIER = 10;
+        for (int k = 0; k < BLOCKS; k++) {
+            for (int i = 1; i < MULTIPLIER+1; i++) {
+                for (int j = INITIAL_VALUE; j < INITIAL_VALUE+COLUMNS; j++) {
+                    System.out.printf("%3d x%2d=%2d",j+COLUMNS*k,i,i*(j+COLUMNS*k));
                 }
                 System.out.println();
             }
