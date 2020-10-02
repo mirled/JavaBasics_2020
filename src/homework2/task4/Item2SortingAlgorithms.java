b77aa2413828743ac726d60c67e4498ff6e00835
Yaroslav Gavrilovets <mir.lednev@gmail.com>
2020-10-02 08:22:21 +0300

    rewrote algorithms for better performance

--- src/homework2/task4/Item2SortingAlgorithms.java
+++ src/homework2/task4/Item2SortingAlgorithms.java
@@ -3,7 +3,6 @@ package homework2.task4;
 import java.util.Arrays;
 
 public class Item2SortingAlgorithms {
-
     static int[] bubbleSort(int[] arr){
         int[] arrSort = Arrays.copyOf(arr,arr.length);
         for (int i = 0; i < arrSort.length-1; i++) {
@@ -20,19 +19,19 @@ public class Item2SortingAlgorithms {
 
     static int[] shakerSort(int[] arr){
         int[] arrSort = Arrays.copyOf(arr,arr.length);
-        for (int i = 0; i < arrSort.length; i++) {
-            for (int j = i; j < arrSort.length-1-i; j++) {
-                if(arrSort[j+1]<arrSort[j]){
-                    int temp = arrSort[j+1];
-                    arrSort[j+1] = arrSort[j];
-                    arrSort[j] = temp;
+        for (int i = 0,k=arrSort.length-1; i < k; i++,k--) {
+            for (int j = i+1; j < k+1; j++) {
+                if(arrSort[j]<arrSort[i]){
+                    int temp = arrSort[j];
+                    arrSort[j] = arrSort[i];
+                    arrSort[i] = temp;
                 }
             }
-            for (int j = arrSort.length-1-i; j > i; j--) {
-                if(arrSort[j-1]>arrSort[j]){
-                    int temp = arrSort[j-1];
-                    arrSort[j-1] = arrSort[j];
-                    arrSort[j] = temp;
+            for (int j = k-1; j > i ; j--) {
+                if(arrSort[j]>arrSort[k]){
+                    int temp = arrSort[j];
+                    arrSort[j] = arrSort[k];
+                    arrSort[k] = temp;
                 }
             }
         }
